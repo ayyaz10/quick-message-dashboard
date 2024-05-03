acfedd2b1ec3f6400016b4bb449651e20297246a
ayyaz10 <codementee@outlook.com>
2024-05-03 23:51:01 +0500

    initial files

---
+++ newTab.tsx
@@ -0,0 +1,9 @@
+const newTab = () => {
+  return (
+    <div>
+      <h1>Quick Message Dashboard</h1>
+    </div>
+  );
+};
+
+export default newTab;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/test.tsx",
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve("src/manifest.json"), to: path.resolve("dist") },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+  },
+};
